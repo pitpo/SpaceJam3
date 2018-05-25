5d4f7648e50e8709474af3276a995fee0c9286dc
Piotr Gabrielski <ppitpo@gmail.com>
2018-05-25 22:17:51 +0200

    Update player movement temporarily and put him in the world

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,45 @@
-extends Area2D
+extends KinematicBody2D
 
 export(int) var MASS
 export(int) var SPEED # will be calculted dynamically from mass in the future
 var velocity = Vector2()
 
+func _init():
+	Util.player = self
+
 func _ready():
+	# spawn next to a planer (earth? XD), orbitting around it
 	pass
 	
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
 	
-	position += velocity * delta
+	# MOVEMENT
+	# use controls to alter the vector
+	
+	# current implementation is temporary
+	if Input.is_action_pressed("ui_right") && velocity.x < 20:
+		velocity.x += 1
+	elif velocity.x > 0:
+		velocity.x -= 1
+		
+	if Input.is_action_pressed("ui_left") && velocity.x > -20:
+		velocity.x -= 1
+	elif velocity.x < 0:
+		velocity.x += 1
+		
+	if Input.is_action_pressed("ui_up") && velocity.y > -20:
+		velocity.y -= 1
+	elif velocity.y < 0:
+		velocity.y += 1
+		
+	if Input.is_action_pressed("ui_down") && velocity.y < 20:
+		velocity.y += 1
+	elif velocity.y > 0:
+		velocity.y -= 1
+	
+	var curVelocity = velocity
+	if velocity.length() > 0:
+		curVelocity = curVelocity.normalized() * SPEED
+	
+	position += curVelocity * delta
 	
