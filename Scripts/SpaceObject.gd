6233b583bd288f458daafbb8b3abf4a6fae8490f
Tomasz Chabora <kobewi4e@gmail.com>
2018-05-25 22:03:25 +0200

    jakieś coś

--- Scripts/SpaceObject.gd
+++ Scripts/SpaceObject.gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
 export var mass = 1
+var motion = Vector2()
 
 func _ready():
 	pass
+
+func _process(delta):
+	var gravity = (Util.player.position - position).normalized()
+	motion += gravity
+	
+	move_and_collide(motion)
