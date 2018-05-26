d5b55b9dcc2d7e13ab53c20a5a70fe1eaf99aeef
Tomasz Chabora <kobewi4e@gmail.com>
2018-05-26 07:01:30 +0200

    różne rzeczy

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -11,8 +11,17 @@ func _ready():
 		cloud.position = Vector2(rand_range(-10000, 10000), rand_range(-10000, 10000))
 		cloud.z_index = rand_range(-100, 0)
 		$Clouds.add_child(cloud)
+	
+	generate_belt(5000)
 
 func _process(delta):
 	background.modulate.r = min(start_color.r + Util.player.position.length() / 10000, 1)
 	background.modulate.b = min(max(start_color.b - (Util.player.position.length() - 5335) / 10000, 0), start_color.b)
 	$Clouds.modulate = background.modulate
+
+func generate_belt(distance):
+	for i in range(500):
+		var trash = preload("res://Nodes/SpaceObjects/SmallTrash.tscn").instance()
+		add_child(trash)
+		var angle = randf() * PI * 2
+		trash.position = Vector2(sin(angle) * distance, cos(angle) * distance)
