d2182ac9bfd1404a0dabc8ec39886234cf1c658f
Tomasz Chabora <kobewi4e@gmail.com>
2018-05-26 02:31:12 +0200

    nowe grafiki

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -40,6 +40,7 @@ func gravitate(object):
 	var dist = object.position - position
 	# no idea what is going on down there but it works
 	if (dist.length() < 3000):
+		print(object.name)
 		var gravity = dist.normalized() * (object.get_node("CollisionShape2D").shape.radius * object.scale.x * 2 / dist.length())
 		if mass/object.mass/10 < 0.05:
 			velocity += gravity * 0.95
