4b9c1c5d0f63444abb389f505980b6a043a77c84
Tomasz Chabora <kobewi4e@gmail.com>
2018-05-26 02:43:44 +0200

    kolor tła

---
+++ Scripts/Game.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+onready var background = $ParallaxBackground/ParallaxLayer/Background
+onready var start_color = background.modulate
+
+func _ready():
+	pass
+
+func _process(delta):
+	background.modulate.r = min(start_color.r + Util.player.position.length() / 10000, 1)
+	background.modulate.b = min(max(start_color.b - (Util.player.position.length() - 5335) / 10000, 0), start_color.b)
