683932cfdcd980449fb0b4154802a8e07da6ec7f
Tomasz Chabora <kobewi4e@gmail.com>
2018-05-26 15:07:00 +0200

    fajne wczytywanie

--- Scripts/Utility.gd
+++ Scripts/Utility.gd
@@ -6,3 +6,4 @@ var textures = {}
 func _ready():
 	textures.Small = [load("res://Sprites/SpaceObjects/SmallRock01.png"), load("res://Sprites/SpaceObjects/SmallRock02.png"), load("res://Sprites/SpaceObjects/SmallRock03.png"), load("res://Sprites/SpaceObjects/AsteroidSmallRock01.png"), load("res://Sprites/SpaceObjects/AsteroidSmallRock02.png"), load("res://Sprites/SpaceObjects/TransitionSmallRock01.png"), load("res://Sprites/SpaceObjects/TransitionSmallRock02.png")]
 	textures.Medium = [load("res://Sprites/SpaceObjects/MediumRock01.png"), load("res://Sprites/SpaceObjects/AsteroidMediumRock01.png"), load("res://Sprites/SpaceObjects/TransitionMediumRock01.png")]
+	textures.planets = [load("res://Sprites/Planets/ExoPlanet.png"), load("res://Sprites/Planets/ExoPlanet02.png"), load("res://Sprites/Planets/ExoPlanet03.png")]
