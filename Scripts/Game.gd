3927b92facb1bb24a89286046dab4ddff7b13df1
Tomasz Chabora <kobewi4e@gmail.com>
2018-05-26 04:24:24 +0200

    chmurt

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -5,7 +5,14 @@ onready var start_color = background.modulate
 
 func _ready():
 	randomize()
+	
+	for i in range(200):
+		var cloud = load("res://Nodes/Cloud.tscn").instance()
+		cloud.position = Vector2(rand_range(-10000, 10000), rand_range(-10000, 10000))
+		cloud.z_index = rand_range(-100, 0)
+		$Clouds.add_child(cloud)
 
 func _process(delta):
 	background.modulate.r = min(start_color.r + Util.player.position.length() / 10000, 1)
 	background.modulate.b = min(max(start_color.b - (Util.player.position.length() - 5335) / 10000, 0), start_color.b)
+	$Clouds.modulate = background.modulate
